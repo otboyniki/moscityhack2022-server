96d91c50490f04537c879378e31d6a564a9887c4
hackerdmitry <hacker.dmitry@gmail.com>
2022-06-12 13:14:06 +0500

    Give me a break, it's 2am. But it works now.

---
+++ Web/Data/Migrations/20220612081331_AddUserFields.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web.Migrations
+{
+    public partial class AddUserFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Education",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string[]>(
+                name: "Languages",
+                table: "Users",
+                type: "text[]",
+                nullable: false,
+                defaultValue: new string[0]);
+
+            migrationBuilder.AddColumn<string[]>(
+                name: "SocialNetworks",
+                table: "Users",
+                type: "text[]",
+                nullable: false,
+                defaultValue: new string[0]);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Education",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Languages",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "SocialNetworks",
+                table: "Users");
+        }
+    }
+}
